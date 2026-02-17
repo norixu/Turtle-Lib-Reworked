cc7b5b8b23459fa5c493ce510fdf14fc3764fe82
rixu <playerscenn@gmail.com>
2026-02-16 23:08:43 -0600

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,44 +1,25 @@
-local UI =
-loadstring(game:HttpGet("https://raw.githubusercontent.com/norixu/eternal-ui/refs/heads/main/source.lua"))()
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/norixu/eternal-ui/refs/heads/main/source.lua"))()
 
-local Window =
-UI:CreateWindow()
+local Window = UI:CreateWindow()
 
-local Tab =
-Window:CreateTab("Main")
+local Tab = Window:CreateTab("Main")
 
-Tab:Button("Notify",function()
-
-UI:Notify("Hello")
-
+Tab:Button("Notification",function()
+UI:Notify("Working")
 end)
 
 Tab:Toggle("Auto Farm",function(v)
-
 print(v)
-
 end)
 
-Tab:Slider("Speed",0,100,function(v)
-
+Tab:Slider("WalkSpeed",0,100,function(v)
 print(v)
-
 end)
 
-Tab:Dropdown("Weapon",{
-
-"Sword",
-"Gun",
-"Bow"
-
-},function(v)
-
+Tab:Dropdown("Weapon",{"Sword","Gun","Bow"},function(v)
 print(v)
-
 end)
 
-Tab:Keybind("Kill Aura",Enum.KeyCode.E,function()
-
+Tab:KeyPicker("Kill Aura",Enum.KeyCode.E,function()
 print("Pressed")
-
 end)
